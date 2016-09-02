b3430b2b06723ace0136f5c7eb415dff20a66c4f
Dalia Banerjee <dalia.banerjee@inmarsat.com>
2016-09-02 17:18:57 +0100

    finished test cases

--- app/scripts/mainControllerSpec.js
+++ app/scripts/mainControllerSpec.js
@@ -79,29 +79,82 @@ describe('controller:mainCtrl', function() {
                 $scope: scope
             });
 
-            $httpBackend.expectGET(url + 'users').respond(200, {data: users});
-            $httpBackend.expectGET(url + 'types').respond(200, {data: types});
-            $httpBackend.expectGET(url + 'files').respond(200, {data: files});
+            $httpBackend.expectGET(url + 'users').respond(200, users);
+            $httpBackend.expectGET(url + 'types').respond(200, types);
+            $httpBackend.expectGET(url + 'files').respond(200, files);
+            $httpBackend.flush();
         });
 
     });
 
-    it('should be able to show all users', inject(function($httpBackend,searchRepository){
-        $httpBackend.expectGET(url + 'users').respond(200, {data: users});
-        searchRepository.findAllUsers().then(function(resp){
-            expect(resp).toEqual(users);
-            expect(scope.numOfUsers).toEqual(2);
-            expect(scope.users).toEqual(users);
-        });
-        $httpBackend.flush();
-    }));
+    it('should be able to show all users', function(){
+        expect(scope.numOfUsers).toEqual(2);
+        expect(scope.users).toEqual(users);
+        expect(scope.users[0].givenName).toBe('Peter');
+    });
+
+    it('should be able to show all live files', function(){
+        expect(scope.allfiles.length).toEqual(1);
+        expect(scope.allfiles[0].title).toBe('Selwebu cu.');
+        expect(scope.allfiles[0].live).toBe(true);
+    });
+
+    it('should be able to show all types', function(){
+        expect(scope.alltypes.length).toEqual(2);
+        expect(scope.alltypes[0].id).toBe('article');
+    });
+
+    it('should be return full name of a person given the id', function(){
+        var name = scope.getName(1);
+        expect(name).toEqual('Peter Capaldi');
+    });
 
     it('should be able to show error message when users returns 500', inject(function($httpBackend,searchRepository){
-        $httpBackend.expectGET(url + 'users').respond(500, {data: []});
+        $httpBackend.expectGET(url + 'users').respond(500, []);
         searchRepository.findAllUsers().then(function(resp){
             expect(resp.length).toEqual(0);
+            expect(scope.error).toBe("Looks like something went wrong in the backend. Please try again");
         });
         $httpBackend.flush();
     }));
 
+    it('should be able to show All files', inject(function($httpBackend){
+        $httpBackend.expectGET(url + 'files').respond(200, files);
+        scope.limit.status = false;
+        scope.showAllFiles();
+        $httpBackend.flush();
+        expect(scope.allfiles.length).toEqual(2);
+        expect(scope.limit.to).toEqual(2);
+    }));
+
+    it('should be able to show Live files', inject(function($httpBackend){
+        $httpBackend.expectGET(url + 'files').respond(200, files);
+        scope.limit.status = false;
+        scope.showLiveFiles();
+        $httpBackend.flush();
+        expect(scope.allfiles.length).toEqual(1);
+        expect(scope.limit.to).toEqual(1);
+    }));
+
+    it('should be able to show Published files', inject(function($httpBackend){
+        $httpBackend.expectGET(url + 'files').respond(200, files);
+        scope.limit.status = false;
+        scope.showPublishedFiles();
+        $httpBackend.flush();
+        expect(scope.allfiles.length).toEqual(1);
+        expect(scope.limit.to).toEqual(1);
+        expect(scope.allfiles[0].status).toBe('Published');
+    }));
+
+    it('should be able to see all data', function(){
+        scope.viewAll();
+        expect(scope.limit.status).toBe(false);
+        expect(scope.limit.to).toBe(1);
+    });
+
+    it('should be able to see less data', function(){
+        scope.viewLess();
+        expect(scope.limit.status).toBe(true);
+        expect(scope.limit.to).toBe(5);
+    });
 });
